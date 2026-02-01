87a32c5fefe20fc1928da34c8a8e58caf2c07205
Aditya Kumar <aditya.work404@gmail.com>
2026-02-01 17:02:41 +0530

    completed courseRouter

--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const courseRouter = express.Router();
+const isLoggdInUser = require('../middlewares/isLoggedInUser')
+const purchaseModel = require('../models/purchaseModel');
+const courseModel = require('../models/courseModel');
 
+courseRouter.get('/purchase',isLoggdInUser,async (req,res) => {
+    let userId = req.user._id
+    let courseId = req.body.courseId;
 
-courseRouter.get('/purchase', (req,res) => {
-    res.send("hell")
+    await purchaseModel.create({
+        userId, courseId
+    })
+    res.json({
+        msg: "you have successfuly purchased",
+        courseId : courseId
+    })
 })
 
-courseRouter.get('/preview', (req,res) => {
-    
+courseRouter.get('/preview',async (req,res) => {
+    let allCourses = await courseModel.find({})
+    res.json({
+         allCourses
+    })
 })
 
 module.exports = courseRouter
